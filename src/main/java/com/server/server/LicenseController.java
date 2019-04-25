cf8f8c8d9620f97cdfc8ef093a32b736243d0114
monarchcheng <monarchcheng1993@gmail.com>
2019-04-25 19:54:31 +0800

    new file:   pom.xml 	new file:   src/main/java/com/server/server/LicenseController.java 	new file:   src/main/java/com/server/server/LicenseUtils.java 	new file:   src/main/java/com/server/server/ServerApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/server/server/ServerApplicationTests.java

---
+++ src/main/java/com/server/server/LicenseController.java
@@ -0,0 +1,46 @@
+package com.server.server;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @ClassName LicenseController
+ * @Description TODO
+ * @Author Kyrie
+ * @Date 2019/4/25 19:17
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping("/rpc")
+public class LicenseController {
+
+    @RequestMapping("/obtainTicket.action")
+    public String obtainTicket(String salt, String userName) throws Exception {
+        String content = "<ObtainTicketResponse>" +
+                "<message></message>" +
+                "<prolongationPeriod>607875500</prolongationPeriod>" +
+                "<responseCode>OK</responseCode>" +
+                "<salt>" + salt + "</salt>" +
+                "<ticketId>1</ticketId>" +
+                "<ticketProperties>licensee=" + userName + "	licenseType=0	</ticketProperties>" +
+                "</ObtainTicketResponse>";
+        return LicenseUtils.rsaSign(content);
+    }
+
+    @RequestMapping("/releaseTicket.action")
+    public void releaseTicket(HttpServletResponse httpServletResponse) {
+        httpServletResponse.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @RequestMapping("ping.action")
+    public String ping(String salt) throws Exception {
+        String content = "<PingResponse>" +
+                "<message></message>" +
+                "<responseCode>OK</responseCode>" +
+                "<salt>" + salt + "</salt>" +
+                "</PingResponse>";
+        return LicenseUtils.rsaSign(content);
+    }
+}
